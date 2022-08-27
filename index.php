941d41924ba37b8dce59f5703066c15dc7b741d2
elvismaicol <elvismaicol@gmail.com>
2022-08-27 15:51:49 -0300

    Minhas auterações

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "35.130.XX.XX";
 $username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$password = "abc123";
+$database = "cadastro";
 
 // Criar conexão
 
